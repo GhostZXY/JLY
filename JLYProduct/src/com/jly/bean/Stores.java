1df2bb783eba4c7f5948c1378e25ad7a4458221a
JiLong <JiLong@DESKTOP-KKNUQ47>
2021-08-26 10:08:39 +0800

    金龙鱼1.3

--- JLYProduct/src/com/jly/bean/Stores.java
+++ JLYProduct/src/com/jly/bean/Stores.java
@@ -1,5 +1,22 @@
 package com.jly.bean;
 
 public class Stores {
+	private int s_id, s_u_id;
+	private String s_name, s_address, s_tel, s_head_old, s_head_new, s_head_status;
 
+	public int getS_id() {
+		return s_id;
+	}
+
+	public void setS_id(int s_id) {
+		this.s_id = s_id;
+	}
+
+	public int getS_u_id() {
+		return s_u_id;
+	}
+
+	public void setS_u_id(int s_u_id) {
+		this.s_u_id = s_u_id;
+	}
 }
